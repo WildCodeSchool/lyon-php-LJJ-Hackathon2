64a526ac00770470846f069637c41ff6716c8a1d
Johan Gaub <johangaub@gmail.com>
2017-04-28 04:11:09 +0200

    operational site with login session

--- src/DiscordBundle/Controller/ChatController.php
+++ src/DiscordBundle/Controller/ChatController.php
@@ -5,6 +5,7 @@ namespace DiscordBundle\Controller;
 use DiscordBundle\Entity\Message;
 use DiscordBundle\Entity\User;
 use DiscordBundle\Form\ChatForm;
+use DiscordBundle\Form\LoginForm;
 use DiscordBundle\Form\RegisterForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -34,18 +35,17 @@ class ChatController extends Controller
             return $this->redirectToRoute('home');
         }
 
-        $session = new Session();
 
         /**
          * Login part
          */
-        if ($form->isSubmitted() && $form->isValid()){
-            $session->start();
+        $session = $request->getSession();
+        $register2 = new User();
+        $form2 = $this->createForm(LoginForm::class, $register2);
+        $form2->handleRequest($request);
+        if ($form2->isSubmitted() && $form2->isValid()){
             $session->set('name', $register->getName());
             $session->getFlashBag()->add('notice', 'Vous êtes connecté !');
-
-            $em->persist($register);
-            $em->flush();
             return $this->redirectToRoute('home');
         }
 
@@ -58,10 +58,10 @@ class ChatController extends Controller
          */
         $display = new Message();
         $display->setDatetime(date('d-m-Y H:i:s'));
-        $form2 = $this->createForm(ChatForm::class, $display);
-        $form2->handleRequest($request);
+        $form3 = $this->createForm(ChatForm::class, $display);
+        $form3->handleRequest($request);
 
-        if ($form->isSubmitted() && $form2->isValid()) {
+        if ($form3->isSubmitted() && $form3->isValid()) {
             $em->persist($display);
             $em->flush();
             return $this->redirectToRoute('home'); /** To be deleted later */
@@ -69,6 +69,7 @@ class ChatController extends Controller
         return $this->render('DiscordBundle:Default:home.html.twig', array(
             'form' => $form->createView(),
             'form2' => $form2->createView(),
+            'form3' => $form3->createView(),
         ));
 
     }
